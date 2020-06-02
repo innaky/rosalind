98c1be7969b8c82a894e2f86334ab4078972212c
Innaky <innaky@protonmail.com>
2020-06-02 09:32:29 -0400

    (doc) adding more documentation in fns.

--- transcribing-dna-to-rna.lisp
+++ transcribing-dna-to-rna.lisp
@@ -1,25 +1,31 @@
 (defun thymine? (dna-chr)
+  "Check if the character is a tymine"
   (if (or (equal #\t dna-chr) (equal #\T dna-chr))
       t
       nil))
 
 (defun match? (elem lst)
+  "General predicate eager False, check if `elem' exists in `lst'."
   (if (equal nil lst)
       nil
       (or (equal elem (car lst))
 	  (match? elem (cdr lst)))))
 
 (defun match-dna? (dna-chr)
+  "Check if the `dna-chr' is a valid DNA character, else return Error"
   (if (match? dna-chr '(#\a #\A #\t #\T #\g #\G #\c #\C))
       (char-upcase dna-chr)
       (error "~A this is not a valid nucleotide." dna-chr)))
 
 (defun t-u (dna-chr)
+  "Transform the thymine to Uracil, match if the another characters are valid and upcase this
+else return a error."
   (if (thymine? dna-chr)
       #\U
       (match-dna? dna-chr)))
 
 (defun dna-rna-lst (dna)
+  "Transform DNA string to RNA but return a list of RNA characters."
   (let ((dna-chars (coerce dna 'list)))
     (if (equal dna-chars nil)
 	nil
@@ -30,8 +36,14 @@
 	 (dna-rna-lst (cdr dna-chars))))))
 
 (defun dna-to-rna (dna)
+  "Take a DNA string and returng a RNA string."
   (coerce (dna-rna-lst dna) 'string))
 
+;; test
+
+;;good
 (dna-to-rna "GATGGAACTTGACTACGTAAATT")
 (dna-to-rna "gatggaacttgactacgtaaatt")
+
+;; bad
 (dna-to-rna "TATAX")
